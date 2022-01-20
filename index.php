77233d7f8df1f534a7f05f157b6835df443b7d3b
vikas soni <vikas.soni@dotsquares.com>
2022-01-20 21:28:43 +0530

    third change

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 
-<?php echo "Hello Docker and Jenkins"; ?>
+<?php echo "Hello Docker and Jenkins !!"; ?>
 <br/><br/>
 <?php $dir = "uploads/*.png";
 //get the list of all files with .jpg extension in the directory and safe it in an array named $images
