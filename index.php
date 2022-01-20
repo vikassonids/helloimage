40e79eb4ff9fc328a55b876286f39e6f4935b7cb
vikas soni <vikas.soni@dotsquares.com>
2022-01-20 20:00:30 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<?php echo "Hello Image"; ?>
+<br/><br/>
+<?php $dir = "uploads/*.png";
+//get the list of all files with .jpg extension in the directory and safe it in an array named $images
+$images = glob( $dir );
+
+//extract only the name of the file without the extension and save in an array named $find
+foreach( $images as $image ):
+    echo "<p><img src='" . $image . "' /></p>";
+endforeach;
+?>
+</body>
+</html>
