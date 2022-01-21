421f922057736d3c385c2495fc9c546542dccffc
vikas soni <vikas.soni@dotsquares.com>
2022-01-21 15:21:00 +0530

    auto delete swarm service and regenerate

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 
-<?php echo "Welcome to CI/CD Pipeline using docker and jenkins !!"; ?>
+<?php echo "Welcome to CI/CD Pipeline tools - Docker and Jenkins !!"; ?>
 <br/><br/>
 <?php $dir = "uploads/*.png";
 //get the list of all files with .jpg extension in the directory and safe it in an array named $images
