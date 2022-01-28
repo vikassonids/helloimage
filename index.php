df7783999fda0b486f3ce9540406393cc91f2d22
vikas soni <vikas.soni@dotsquares.com>
2022-01-28 13:58:52 +0530

    text changes

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 
-<?php echo "Hello All, Welcome to CI/CD Pipeline tools - Docker and Jenkins !!"; ?>
+<?php echo "Hello, Welcome to CI/CD Pipeline tools - Docker and Jenkins !!"; ?>
 <br/><br/>
 <?php $dir = "uploads/*.png";
 //get the list of all files with .jpg extension in the directory and safe it in an array named $images
